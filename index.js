a1273aa2a05b9daa3243dfb0b4a9feed493422b6
Veronica Bustabad <vbustabad@gmail.com>
2018-09-01 16:27:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function scuberGreetingForFeet(feet) {
 }
 
 function ternaryCheckCity(city) {
-  return city === "NYC" ? "Ok, sounds good." : "No go."
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
+}
 
 function switchOnCharmFromTip(tip) {
   let result

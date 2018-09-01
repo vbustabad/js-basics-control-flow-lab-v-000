ea719136624af0b0dd8aa2d969cd627b13662b79
Veronica Bustabad <vbustabad@gmail.com>
2018-09-01 16:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(feet) {
+function scuberGreetingForFeet (feet) {
   let result
   if (feet <= 400) {
     result = "This one is on me!";

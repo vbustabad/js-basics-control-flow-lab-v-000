53b4409dcd012113ee224e7a01706a5a604dcea5
Veronica Bustabad <vbustabad@gmail.com>
2018-09-01 16:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(ride) {
   let result
   if (ride <= 400) {
     result = "This one is on me!";
-  } else if (ride > 400 && ride < 2500) {
+  } else if (ride > 400 && ride <= 2500) {
     result = "I will gladly take your thirty bucks.";
   } else if (ride > 2500) {
     result = "No can do.";

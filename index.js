df69f2bac2ea752d95f36b5cbca2edf20582fd1f
Veronica Bustabad <vbustabad@gmail.com>
2018-09-01 04:39:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(ride) {
+  let result
+  if (ride <= 400) {
+    result = "This one is on me!";
+  } else if (ride > 2000) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (ride > 2500) {
+    result = "No can do.";
+  {
+  return result
+}
+
+function ternaryCheckCity(city) {
+  let result
+  if (city === "NYC") {
+    result = "Ok, sounds good.";
+  } else {
+    result = "No go.";
+  }
+  return result
+}
+
+function switchOnCharmFromTip(tip) {
+  let result
+  if (tip === "generous") {
+    result = "Thank you so much.";
+  } else if (ride > 2000) {
+    result = "Thank you.";
+  }
+  else {
+    result = "Bye.";
+  }
+  return result
+}
